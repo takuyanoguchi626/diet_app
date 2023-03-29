47dd5d6ed9fcc7e8e285c05a398e1a0bd9510023
takuyanoguchi626 <ngctky626@gmail.com>
2023-03-30 00:47:20 +0900

    create fontfile normalize.scss

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -4,14 +4,14 @@ import Layout from '../components/Layout.tsx/Layout'
 export default function Top() {
   return (
     <Layout>
-        <div>
-          <div className='title_16'>今日のカロリー目安</div>
-          <div className='l_flex'>
-          <span className="material-symbols-rounded">restaurant</span>
-            <div>食事の目安</div>
-            <div>3000kcal</div>
+        <div className='pt_24 pr_16 pl_16 top'>
+          <div className='title_18 text_center'>今日のカロリー目安</div>
+          <div className='l_flex card'>
+            <span className="material-symbols-rounded card_icon">restaurant</span>
+            <div className='ml_12 card_text'>食事の目安</div>
+            <div className='ml_at card_text'>3000kcal</div>
           </div>
-          <div className='title_16'>今日のカロリー目安</div>
+          <div className='title_18 text_center'>今日のカロリー計画</div>
         </div>
     </Layout>
   )
